a0815ab9f6e254c78baab32934c85fae87be51d1
Nadya Yarmak <91716602+NadyaYar@users.noreply.github.com>
2022-11-21 22:50:23 +0200

    Update task7.cpp

--- 26.09(4)/task7.cpp
+++ 26.09(4)/task7.cpp
@@ -1,14 +1,14 @@
 #include <iostream> 
 using namespace std; 
 int main(void){ 
-    float a1= 2.3; 
-    float a2= 2.3; 
-    float a3= 2.123456; 
-    float a4= 2.123456; 
-    float a5= 2.123456; 
-    cout << a1 <<'\n'; 
-    cout << a2 <<"0"<<'\n'; 
-    cout << a3 <<'\n'; 
-    cout << round (a4 * 100)/100 <<'\n'; 
-    cout << int (a5) <<'\n'; 
+    float num1= 2.3; 
+    float num2= 2.3; 
+    float num3= 2.123456; 
+    float num4= 2.123456; 
+    float num5= 2.123456; 
+    cout << num1 <<'\n'; 
+    cout << num2 <<"0"<<'\n'; 
+    cout << num3 <<'\n'; 
+    cout << round (num4 * 100)/100 <<'\n'; 
+    cout << int (num5) <<'\n'; 
 }
