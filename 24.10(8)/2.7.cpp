fd62a7cd985dced17f4600baae456f5103f7a3f0
Nadya Yarmak <91716602+NadyaYar@users.noreply.github.com>
2022-11-21 23:02:39 +0200

    Update 2.7.cpp

--- 24.10(8)/2.7.cpp
+++ 24.10(8)/2.7.cpp
@@ -4,14 +4,14 @@ using namespace std;
 int main(void) { 
   double pi4 = 0.; 
   long n; 
-  long j = 1; 
+  long k = 1; 
    
   cout << "Number of iterations? "; 
   cin >> n; 
    
   for(int i=0; i<n; i=i+2){ 
-    pi4 = pi4 + ((1./j)-(1./(j+2))); 
-    j=j+4; 
+    pi4 = pi4 + ((1./k)-(1./(k+2))); 
+    k=k+4; 
   } 
    
   cout.precision(20); 
